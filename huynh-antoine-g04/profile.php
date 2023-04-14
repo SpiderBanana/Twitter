ff86d12941b91b38906221b682ec6286f93d68ab
Antoine Huynh <90709220+SpiderBanana@users.noreply.github.com>
2023-04-14 23:14:49 +0200

    Add files via upload

---
+++ huynh-antoine-g04/profile.php
@@ -0,0 +1,173 @@
+<?php
+session_start();
+
+// Connexion à la base de données
+$pdo = new PDO('mysql:host=localhost;dbname=projet', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+$tags = [
+    '1' => 'Sport',
+    '2' => 'Culture',
+    '3' => 'JeuxVidéo',
+    '4' => 'Histoire',
+    '5' => 'Cinéma',
+    '6' => 'Littérature',
+    '7' => 'Tech',
+    '8' => 'Musique',
+    '9' => 'Animé',
+    '10' => 'Sondage',
+  ];
+  
+  // Tableau des couleurs des tags
+  $tagColors = [
+    '1' => '#F44336',
+    '2' => '#E91E63',
+    '3' => '#9C27B0',
+    '4' => '#673AB7',
+    '5' => '#3F51B5',
+    '6' => '#2196F3',
+    '7' => '#03A9F4',
+    '8' => '#00BCD4',
+    '9' => '#009688',
+    '10' => '#4CAF50',
+  ];
+  
+
+$user = isset($_GET['user']) ? $_GET['user'] : '';
+if (empty($user)) {
+    header('Location: home.php');
+    exit;
+}
+
+function getUserProfilePicture($pdo, $pseudo) {
+  $stmt = $pdo->prepare("SELECT profile_picture FROM users WHERE pseudo = :pseudo");
+  $stmt->execute(['pseudo' => $pseudo]);
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+  return $user ? $user['profile_picture'] : '';
+}
+
+$stmt = $pdo->prepare("SELECT * FROM posts WHERE pseudo = :pseudo ORDER BY date_heure_message DESC");
+$stmt->execute(['pseudo' => $user]);
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>profil de <?php echo htmlspecialchars($user);?></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> <!--importation Bootstrap-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <!--importation jquery-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> <!--importation ajax-->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> <!--importation Bootstrap-->
+  <link rel="stylesheet" href="css/home_style.css">
+  <link rel="stylesheet" href="css/dark_theme.css">
+  <link rel="stylesheet" href="css/button.css">
+  
+  
+</head>
+<div class="page-content">
+    <div class="page-content">
+
+      
+        <div class="container">
+          
+        <!-- boutton dark theme -->
+        <div class="toggle-theme-container d-flex justify-content-center">
+            <button id="toggle-theme" class="btn btn-primary">Passer en mode nuit</button>
+          </div>
+        <!-- boutton deconnexion --> 
+        <div class="container-btn">
+            <a href="home.php" class="btn btn-secondary">◀️</a>
+
+            
+
+        <div class="settings-container">
+
+        </div>
+          </div>
+        <!-- bienvenue, --> 
+          <div class="container-wrapper">
+      <div class="welcome-container">
+        <?php if (isset($_SESSION['pseudo'])) : ?>
+          <img src="<?php echo (htmlspecialchars(getUserProfilePicture($pdo, $_SESSION['pseudo'])) ?: 'default_profile_picture.png') . '?t=' . time(); ?>" alt="Profile Picture" class="profile-picture" />
+          <h1 class="my-4 welcome-message">Bienvenue, <?php echo htmlspecialchars($_SESSION['pseudo']); ?></h1>
+        <?php else : ?>
+          <h1 class="my-4 welcome-message">Bienvenue</h1>
+        <?php endif; ?>
+      </div>
+
+
+            
+          </div>
+          <!-- Affichage "Posts" -->
+      <h2 class="my-4 centered">Posts de <?php echo htmlspecialchars($user);?></h2>
+
+      <div class="tweets-container">
+          <form method="get" class="search-form mb-3" id="search-form">
+
+        <div class="input-group">
+
+      </form>
+</div>
+</form>
+</div>
+          
+
+<?php
+      // On affiche les posts triés selon l'ordre choisi
+      while ($post = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<div class="post-container d-flex">';
+    
+        // Afficher la photo de profil en haut à gauche avec le lien vers le profil
+        $userProfilePicture = getUserProfilePicture($pdo, $post['pseudo']);
+        if (empty($userProfilePicture)) {
+            $userProfilePicture = 'default_profile_picture.png';
+        }
+        echo '<a href="profile.php?user=' . htmlspecialchars($post['pseudo']) . '">';
+        echo '<div class="post-profile-picture">';
+        echo '<img src="' . htmlspecialchars($userProfilePicture) . '" alt="Profile Picture" class="profile-picture" />';
+        echo '</div>';
+        echo '</a>';
+    
+        // Afficher le pseudo avec la date et heure juste en dessous
+        echo '<div class="post-content-container">';
+        echo '<strong class="post-username">' . $post['pseudo'] . ' <span style="color: ' . $tagColors[$post['tag']] . '">[' . $tags[$post['tag']] . ']</span></strong>';
+        echo '<div class="post-info d-flex">';
+        echo '<small>Date et heure: ' . date('d-m-Y H:i:s', strtotime($post['date_heure_message'])) . '</small>';
+        echo '</div>';
+        
+        // Afficher le contenu du post
+        echo '<p class="post-content" style="word-wrap: break-word;">' . $post['content'] . '</p>';
+        
+        // Afficher l'image du post s'il y en a une
+        if (!empty($post['image_path'])) {
+          echo '<div class="post-image">';
+          echo '<img src="' . htmlspecialchars($post['image_path']) . '" alt="Post Image" />';
+          echo '</div>';
+        }
+        
+        echo '</div>';
+        
+        // bouton supprimer
+        echo '<div class="post-delete-button">';
+        if (isset($_SESSION['pseudo']) && $_SESSION['pseudo'] == $post['pseudo']) {
+          echo '<form method="post" style="display: inline;">';
+          echo '<input type="hidden" name="post_id" value="' . $post['id'] . '">';
+          echo '<button type="submit" class="btn btn-danger btn-sm" name="delete_post" onclick="return confirmDelete();">&#128465</button>';
+          echo '</form>';
+        }
+        
+        echo '</div>';
+        
+        echo '</div>';
+    }
+    
+      ?>
+          </ul>
+</body>
+</html>
+
+
+<script src="script.js"></script>
+
